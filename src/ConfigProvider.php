160f4805cf2a8b0b477fe399477135838bd119fb
Rex <chep6915@gmail.com>
2026-07-03 17:45:44 +0800

    Fix bug
    Fix bug

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -14,13 +14,13 @@ class ConfigProvider
                 [
                     'id' => 'migration',
                     'description' => 'The migrations for hyperf-scheduled-task.',
-                    'source' => __DIR__ . '/migrations/create_scheduled_tasks_table.php', // 你套件內的源路徑
+                    'source' => __DIR__ . '/migrations/2026_07_03_000001_create_scheduled_tasks_table.php', // 你套件內的源路徑
                     'destination' => BASE_PATH . '/migrations/' . date('Y_m_d_His') . '_create_scheduled_tasks_table.php', // 目標專案路徑
                 ],
                 [
                     'id' => 'migration_log',
                     'description' => 'The migrations for hyperf-scheduled-task execution logs.',
-                    'source' => __DIR__ . '/../migrations/create_task_execution_logs_table.php',
+                    'source' => __DIR__ . '/migrations/2026_07_03_000002_create_task_execution_logs_table.php',
                     'destination' => BASE_PATH . '/migrations/' . date('Y_m_d_His', time() + 1) . '_create_task_execution_logs_table.php',
                 ],
             ],
